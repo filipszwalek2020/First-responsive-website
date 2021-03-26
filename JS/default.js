c0ad96582ddba2a32048a87ba557d8062ce7a156
Maciej Szwałek <maciejfilip.szwalek@gmail.com>
2021-03-26 23:24:38 +0100

    Stylizacja paska scrollbar, zmiana kodu JS po testach

---
+++ JS/default.js
@@ -0,0 +1,71 @@
+/*
+var k1=document.getElementById('k1');
+var k2=document.getElementById('k2');
+var k3=document.getElementById('k3');
+var k4=document.getElementById('k4');
+var k5=document.getElementById('k5');
+var k6=document.getElementById('k6');
+var k7=document.getElementById('k7');
+var k8=document.getElementById('k8');
+var k9=document.getElementById('k9');
+var k10=document.getElementById('k10');
+var k11=document.getElementById('k11');
+var k12=document.getElementById('k12');
+var k13=document.getElementById('k13');
+var k14=document.getElementById('k14');
+var k15=document.getElementById('k15');
+var k16=document.getElementById('k16');
+var k17=document.getElementById('k17');
+var k18=document.getElementById('k18');
+var k19=document.getElementById('k19');
+var k20=document.getElementById('k20');
+
+k1.addEventListener("click",function(){Opis(1);});
+k2.addEventListener("click",function(){Opis(2);});
+k3.addEventListener("click",function(){Opis(3);});
+k4.addEventListener("click",function(){Opis(4);});
+k5.addEventListener("click",function(){Opis(5);});
+k6.addEventListener("click",function(){Opis(6);});
+k7.addEventListener("click",function(){Opis(7);});
+k8.addEventListener("click",function(){Opis(8);});
+k9.addEventListener("click",function(){Opis(9);});
+k10.addEventListener("click",function(){Opis(10);});
+k11.addEventListener("click",function(){Opis(11);});
+k12.addEventListener("click",function(){Opis(12);});
+k13.addEventListener("click",function(){Opis(13);});
+k14.addEventListener("click",function(){Opis(14);});
+k15.addEventListener("click",function(){Opis(15);});
+k16.addEventListener("click",function(){Opis(16);});
+k17.addEventListener("click",function(){Opis(17);});
+k18.addEventListener("click",function(){Opis(18);});
+k19.addEventListener("click",function(){Opis(19);});
+k20.addEventListener("click",function(){Opis(20);});
+*/
+
+
+function Opis(n)
+{
+   
+    
+   
+a="IMG/DONE/"+n+".png";
+     
+//a="k"+n;    
+//document.getElementById(a).innerHTML=a;    
+
+
+
+$("k"+n).attr("src", "IMG/DONE/8.png");
+        
+    
+    
+}
+
+
+function Odwroc(n)
+{
+ console.log("eee:"+n);     
+    
+    
+    
+}
