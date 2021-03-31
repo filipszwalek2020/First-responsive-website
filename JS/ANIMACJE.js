19389b1494766a056780f08f454d9487d4dd17c1
Maciej Szwałek <maciejfilip.szwalek@gmail.com>
2021-03-31 16:27:04 +0200

    Poprawka CSS oraz zmiana animacji kafelków

---
+++ JS/ANIMACJE.js
@@ -0,0 +1,368 @@
+$(function() {
+    $("#k1").mouseover(function() {
+        $("#k1").find('#k1').animate({transform: "scale(1)"}, 5000, 'linear');
+        $("#k1").find('.overlayT').toggleClass('animate'); 
+    })});
+$(function() {
+    $("#k2").mouseover(function() {
+        $("#k2").find('#k2').animate({transform: "scale(1)"}, 5000, 'linear');
+        $("#k2").find('.overlayT').toggleClass('animate'); 
+    })});
+$(function() {
+    $("#k3").mouseover(function() {
+        $("#k3").find('#k3').animate({transform: "scale(1)"}, 5000, 'linear');
+        $("#k3").find('.overlayT').toggleClass('animate'); 
+    })});
+$(function() {
+    $("#k4").mouseover(function() {
+        $("#k4").find('#k4').animate({transform: "scale(1)"}, 5000, 'linear');
+        $("#k4").find('.overlayT').toggleClass('animate'); 
+    })});
+$(function() {
+    $("#k5").mouseover(function() {
+        $("#k5").find('#k5').animate({transform: "scale(1)"}, 5000, 'linear');
+        $("#k5").find('.overlayT').toggleClass('animate'); 
+    })});
+$(function() {
+    $("#k6").mouseover(function() {
+        $("#k6").find('#k6').animate({transform: "scale(1)"}, 5000, 'linear');
+        $("#k6").find('.overlayT').toggleClass('animate'); 
+    })});
+$(function() {
+    $("#k7").mouseover(function() {
+        $("#k7").find('#k7').animate({transform: "scale(1)"}, 5000, 'linear');
+        $("#k7").find('.overlayT').toggleClass('animate'); 
+    })});
+$(function() {
+    $("#k8").mouseover(function() {
+        $("#k8").find('#k8').animate({transform: "scale(1)"}, 5000, 'linear');
+        $("#k8").find('.overlayT').toggleClass('animate'); 
+    })});
+$(function() {
+    $("#k9").mouseover(function() {
+        $("#k9").find('#k9').animate({transform: "scale(1)"}, 5000, 'linear');
+        $("#k9").find('.overlayT').toggleClass('animate'); 
+    })});
+$(function() {
+    $("#k10").mouseover(function() {
+        $("#k10").find('#k10').animate({transform: "scale(1)"}, 5000, 'linear');
+        $("#k10").find('.overlayT').toggleClass('animate'); 
+    })});
+$(function() {
+    $("#k11").mouseover(function() {
+        $("#k11").find('#k11').animate({transform: "scale(1)"}, 5000, 'linear');
+        $("#k11").find('.overlayT').toggleClass('animate'); 
+    })});
+$(function() {
+    $("#k12").mouseover(function() {
+        $("#k12").find('#k12').animate({transform: "scale(1)"}, 5000, 'linear');
+        $("#k12").find('.overlayT').toggleClass('animate'); 
+    })});
+$(function() {
+    $("#k13").mouseover(function() {
+        $("#k13").find('#k13').animate({transform: "scale(1)"}, 5000, 'linear');
+        $("#k13").find('.overlayT').toggleClass('animate'); 
+    })});
+$(function() {
+    $("#k14").mouseover(function() {
+        $("#k14").find('#k14').animate({transform: "scale(1)"}, 5000, 'linear');
+        $("#k14").find('.overlayT').toggleClass('animate'); 
+    })});
+$(function() {
+    $("#k15").mouseover(function() {
+        $("#k15").find('#k15').animate({transform: "scale(1)"}, 5000, 'linear');
+        $("#k15").find('.overlayT').toggleClass('animate'); 
+    })});
+$(function() {
+    $("#k16").mouseover(function() {
+        $("#k16").find('#k16').animate({transform: "scale(1)"}, 5000, 'linear');
+        $("#k16").find('.overlayT').toggleClass('animate'); 
+    })});
+$(function() {
+    $("#k17").mouseover(function() {
+        $("#k17").find('#k17').animate({transform: "scale(1)"}, 5000, 'linear');
+        $("#k17").find('.overlayT').toggleClass('animate'); 
+    })});
+$(function() {
+    $("#k18").mouseover(function() {
+        $("#k18").find('#k18').animate({transform: "scale(1)"}, 5000, 'linear');
+        $("#k18").find('.overlayT').toggleClass('animate'); 
+    })});
+$(function() {
+    $("#k19").mouseover(function() {
+        $("#k19").find('#k19').animate({transform: "scale(1)"}, 5000, 'linear');
+        $("#k19").find('.overlayT').toggleClass('animate'); 
+    })});
+$(function() {
+    $("#k20").mouseover(function() {
+        $("#k20").find('#k20').animate({transform: "scale(1)"}, 5000, 'linear');
+        $("#k20").find('.overlayT').toggleClass('animate'); 
+    })});
+// on mouseout
+$(function() {
+    $("#k1").mouseout(function() {
+        $("#k1").find('#k1').animate({transform: "scale(1)"}, 5000, 'linear');
+        $("#k1").find('.overlayT').toggleClass('animate'); 
+    })});
+$(function() {
+    $("#k2").mouseout(function() {
+        $("#k2").find('#k2').animate({transform: "scale(1)"}, 5000, 'linear');
+        $("#k2").find('.overlayT').toggleClass('animate'); 
+    })});
+$(function() {
+    $("#k3").mouseout(function() {
+        $("#k3").find('#k3').animate({transform: "scale(1)"}, 5000, 'linear');
+        $("#k3").find('.overlayT').toggleClass('animate'); 
+    })});
+$(function() {
+    $("#k4").mouseout(function() {
+        $("#k4").find('#k4').animate({transform: "scale(1)"}, 5000, 'linear');
+        $("#k4").find('.overlayT').toggleClass('animate'); 
+    })});
+$(function() {
+    $("#k5").mouseout(function() {
+        $("#k5").find('#k5').animate({transform: "scale(1)"}, 5000, 'linear');
+        $("#k5").find('.overlayT').toggleClass('animate'); 
+    })});
+$(function() {
+    $("#k6").mouseout(function() {
+        $("#k6").find('#k6').animate({transform: "scale(1)"}, 5000, 'linear');
+        $("#k6").find('.overlayT').toggleClass('animate'); 
+    })});
+$(function() {
+    $("#k7").mouseout(function() {
+        $("#k7").find('#k7').animate({transform: "scale(1)"}, 5000, 'linear');
+        $("#k7").find('.overlayT').toggleClass('animate'); 
+    })});
+$(function() {
+    $("#k8").mouseout(function() {
+        $("#k8").find('#k8').animate({transform: "scale(1)"}, 5000, 'linear');
+        $("#k8").find('.overlayT').toggleClass('animate'); 
+    })});
+$(function() {
+    $("#k9").mouseout(function() {
+        $("#k9").find('#k9').animate({transform: "scale(1)"}, 5000, 'linear');
+        $("#k9").find('.overlayT').toggleClass('animate'); 
+    })});
+$(function() {
+    $("#k10").mouseout(function() {
+        $("#k10").find('#k10').animate({transform: "scale(1)"}, 5000, 'linear');
+        $("#k10").find('.overlayT').toggleClass('animate'); 
+    })});
+$(function() {
+    $("#k11").mouseout(function() {
+        $("#k11").find('#k11').animate({transform: "scale(1)"}, 5000, 'linear');
+        $("#k11").find('.overlayT').toggleClass('animate'); 
+    })});
+$(function() {
+    $("#k12").mouseout(function() {
+        $("#k12").find('#k12').animate({transform: "scale(1)"}, 5000, 'linear');
+        $("#k12").find('.overlayT').toggleClass('animate'); 
+    })});
+$(function() {
+    $("#k13").mouseout(function() {
+        $("#k13").find('#k13').animate({transform: "scale(1)"}, 5000, 'linear');
+        $("#k13").find('.overlayT').toggleClass('animate'); 
+    })});
+$(function() {
+    $("#k14").mouseout(function() {
+        $("#k14").find('#k14').animate({transform: "scale(1)"}, 5000, 'linear');
+        $("#k14").find('.overlayT').toggleClass('animate'); 
+    })});
+$(function() {
+    $("#k15").mouseout(function() {
+        $("#k15").find('#k15').animate({transform: "scale(1)"}, 5000, 'linear');
+        $("#k15").find('.overlayT').toggleClass('animate'); 
+    })});
+$(function() {
+    $("#k16").mouseout(function() {
+        $("#k16").find('#k16').animate({transform: "scale(1)"}, 5000, 'linear');
+        $("#k16").find('.overlayT').toggleClass('animate'); 
+    })});
+$(function() {
+    $("#k17").mouseout(function() {
+        $("#k17").find('#k17').animate({transform: "scale(1)"}, 5000, 'linear');
+        $("#k17").find('.overlayT').toggleClass('animate'); 
+    })});
+$(function() {
+    $("#k18").mouseout(function() {
+        $("#k18").find('#k18').animate({transform: "scale(1)"}, 5000, 'linear');
+        $("#k18").find('.overlayT').toggleClass('animate'); 
+    })});
+$(function() {
+    $("#k19").mouseout(function() {
+        $("#k19").find('#k19').animate({transform: "scale(1)"}, 5000, 'linear');
+        $("#k19").find('.overlayT').toggleClass('animate'); 
+    })});
+$(function() {
+    $("#k20").mouseout(function() {
+        $("#k20").find('#k20').animate({transform: "scale(1)"}, 5000, 'linear');
+        $("#k20").find('.overlayT').toggleClass('animate'); 
+    })});
+
+
+//On mouseover                                    
+$(function() {
+    $("#k1").mouseover(function() {
+        $('#k1').find('.link').css({opacity:0.90});
+        })});
+$(function() {
+    $("#k2").mouseover(function() {
+        $('#k2').find('.link').css({opacity:0.90});
+        })});
+$(function() {
+    $("#k3").mouseover(function() {
+        $('#k3').find('.link').css({opacity:0.90});
+        })});
+$(function() {
+    $("#k4").mouseover(function() {
+        $('#k4').find('.link').css({opacity:0.90});
+        })});
+$(function() {
+    $("#k5").mouseover(function() {
+        $('#k5').find('.link').css({opacity:0.90});
+        })});
+$(function() {
+    $("#k6").mouseover(function() {
+        $('#k6').find('.link').css({opacity:0.90});
+        })});
+$(function() {
+    $("#k7").mouseover(function() {
+        $('#k7').find('.link').css({opacity:0.90});
+        })});
+$(function() {
+    $("#k8").mouseover(function() {
+        $('#k8').find('.link').css({opacity:0.90});
+        })});        
+$(function() {
+    $("#k9").mouseover(function() {
+        $('#k9').find('.link').css({opacity:0.90});
+        })});
+$(function() {
+    $("#k10").mouseover(function() {
+        $('#k10').find('.link').css({opacity:0.90});
+        })});
+$(function() {
+    $("#k11").mouseover(function() {
+        $('#k11').find('.link').css({opacity:0.90});
+        })});
+$(function() {
+    $("#k12").mouseover(function() {
+        $('#k12').find('.link').css({opacity:0.90});
+        })});
+$(function() {
+    $("#k13").mouseover(function() {
+        $('#k13').find('.link').css({opacity:0.90});
+        })});
+$(function() {
+    $("#k14").mouseover(function() {
+        $('#k14').find('.link').css({opacity:0.90});
+        })});        
+$(function() {
+    $("#k15").mouseover(function() {
+        $('#k15').find('.link').css({opacity:0.90});
+        })});
+$(function() {
+    $("#k16").mouseover(function() {
+        $('#k16').find('.link').css({opacity:0.90});
+        })});
+$(function() {
+    $("#k17").mouseover(function() {
+        $('#k17').find('.link').css({opacity:0.90});
+        })});       
+$(function() {
+    $("#k18").mouseover(function() {
+        $('#k18').find('.link').css({opacity:0.90});
+        })});
+$(function() {
+    $("#k19").mouseover(function() {
+        $('#k19').find('.link').css({opacity:0.90});
+        })});
+$(function() {
+    $("#k20").mouseover(function() {
+        $('#k20').find('.link').css({opacity:0.90});
+        })});        
+              
+//On mouseout
+$(function() {
+    $("#k1").mouseout(function() {
+        $('#k1').find('.link').css({opacity:0});
+        })});
+$(function() {
+    $("#k2").mouseout(function() {
+        $('#k2').find('.link').css({opacity:0});
+        })});
+$(function() {
+    $("#k3").mouseout(function() {
+        $('#k3').find('.link').css({opacity:0});
+        })});
+$(function() {
+    $("#k4").mouseout(function() {
+        $('#k4').find('.link').css({opacity:0});
+        })});
+$(function() {
+    $("#k5").mouseout(function() {
+        $('#k5').find('.link').css({opacity:0});
+        })});
+$(function() {
+    $("#k6").mouseout(function() {
+        $('#k6').find('.link').css({opacity:0});
+        })});
+$(function() {
+    $("#k7").mouseout(function() {
+        $('#k7').find('.link').css({opacity:0});
+        })});
+$(function() {
+    $("#k8").mouseout(function() {
+        $('#k8').find('.link').css({opacity:0});
+        })});        
+$(function() {
+    $("#k9").mouseout(function() {
+        $('#k9').find('.link').css({opacity:0});
+        })});
+$(function() {
+    $("#k10").mouseout(function() {
+        $('#k10').find('.link').css({opacity:0});
+        })});
+$(function() {
+    $("#k11").mouseout(function() {
+        $('#k11').find('.link').css({opacity:0});
+        })});
+$(function() {
+    $("#k12").mouseout(function() {
+        $('#k12').find('.link').css({opacity:0});
+        })});
+$(function() {
+    $("#k13").mouseout(function() {
+        $('#k13').find('.link').css({opacity:0});
+        })});
+$(function() {
+    $("#k14").mouseout(function() {
+        $('#k14').find('.link').css({opacity:0});
+        })});        
+$(function() {
+    $("#k15").mouseout(function() {
+        $('#k15').find('.link').css({opacity:0});
+        })});
+$(function() {
+    $("#k16").mouseout(function() {
+        $('#k16').find('.link').css({opacity:0});
+        })});
+$(function() {
+    $("#k17").mouseout(function() {
+        $('#k17').find('.link').css({opacity:0});
+        })});       
+$(function() {
+    $("#k18").mouseout(function() {
+        $('#k18').find('.link').css({opacity:0});
+        })});
+$(function() {
+    $("#k19").mouseout(function() {
+        $('#k19').find('.link').css({opacity:0});
+        })});
+$(function() {
+    $("#k20").mouseout(function() {
+        $('#k20').find('.link').css({opacity:0});
+        })});  
+
+
--- JS/default.js
+++ JS/default.js
@@ -1,71 +1,72 @@
-/*
-var k1=document.getElementById('k1');
-var k2=document.getElementById('k2');
-var k3=document.getElementById('k3');
-var k4=document.getElementById('k4');
-var k5=document.getElementById('k5');
-var k6=document.getElementById('k6');
-var k7=document.getElementById('k7');
-var k8=document.getElementById('k8');
-var k9=document.getElementById('k9');
-var k10=document.getElementById('k10');
-var k11=document.getElementById('k11');
-var k12=document.getElementById('k12');
-var k13=document.getElementById('k13');
-var k14=document.getElementById('k14');
-var k15=document.getElementById('k15');
-var k16=document.getElementById('k16');
-var k17=document.getElementById('k17');
-var k18=document.getElementById('k18');
-var k19=document.getElementById('k19');
-var k20=document.getElementById('k20');
-
-k1.addEventListener("click",function(){Opis(1);});
-k2.addEventListener("click",function(){Opis(2);});
-k3.addEventListener("click",function(){Opis(3);});
-k4.addEventListener("click",function(){Opis(4);});
-k5.addEventListener("click",function(){Opis(5);});
-k6.addEventListener("click",function(){Opis(6);});
-k7.addEventListener("click",function(){Opis(7);});
-k8.addEventListener("click",function(){Opis(8);});
-k9.addEventListener("click",function(){Opis(9);});
-k10.addEventListener("click",function(){Opis(10);});
-k11.addEventListener("click",function(){Opis(11);});
-k12.addEventListener("click",function(){Opis(12);});
-k13.addEventListener("click",function(){Opis(13);});
-k14.addEventListener("click",function(){Opis(14);});
-k15.addEventListener("click",function(){Opis(15);});
-k16.addEventListener("click",function(){Opis(16);});
-k17.addEventListener("click",function(){Opis(17);});
-k18.addEventListener("click",function(){Opis(18);});
-k19.addEventListener("click",function(){Opis(19);});
-k20.addEventListener("click",function(){Opis(20);});
-*/
-
-
-function Opis(n)
+function Circles(n)
 {
-   
-    
-   
-a="IMG/DONE/"+n+".png";
-     
-//a="k"+n;    
-//document.getElementById(a).innerHTML=a;    
+var kola='';
+                                                         
+switch(n)
+    {
+    //C++    
+    case 1:kola=kola+'<i class="demo-icon icon-circle"></i>'+'<i class="demo-icon icon-circle-empty"></i><i class="demo-icon icon-circle-empty"></i><i class="demo-icon icon-circle-empty"></i><i class="demo-icon icon-circle-empty"></i>';break;       
 
+    //JavaScript
+    case 2:kola=kola+'<i class="demo-icon icon-circle"></i>'+'<i class="demo-icon icon-circle-empty"></i><i class="demo-icon icon-circle-empty"></i><i class="demo-icon icon-circle-empty"></i><i class="demo-icon icon-circle-empty"></i>';break;
 
+    //Bootstrap    
+    case 3:kola=kola+'<i class="demo-icon icon-circle"></i>'+'<i class="demo-icon icon-circle-empty"></i><i class="demo-icon icon-circle-empty"></i><i class="demo-icon icon-circle-empty"></i><i class="demo-icon icon-circle-empty"></i>';break;  
 
-$("k"+n).attr("src", "IMG/DONE/8.png");
-        
+    //Python 
+    case 4:kola=kola+'<i class="demo-icon icon-circle"></i>'+'<i class="demo-icon icon-circle-empty"></i><i class="demo-icon icon-circle-empty"></i><i class="demo-icon icon-circle-empty"></i><i class="demo-icon icon-circle-empty"></i>';break;  
+
+    //Adobe Illustrator    
+    case 5:kola=kola+'<i class="demo-icon icon-circle"></i><i class="demo-icon icon-circle"></i>'+'<i class="demo-icon icon-circle-empty"></i><i class="demo-icon icon-circle-empty"></i><i class="demo-icon icon-circle-empty"></i>';break;  
+
+    //Adobe Photoshop     
+    case 6:kola=kola+'<i class="demo-icon icon-circle"></i><i class="demo-icon icon-circle"></i><i class="demo-icon icon-circle"></i>'+'<i class="demo-icon icon-circle-empty"></i><i class="demo-icon icon-circle-empty"></i>';break; 
+
+    //Adobe Premiere Pro
+    case 7:kola=kola+'<i class="demo-icon icon-circle"></i><i class="demo-icon icon-circle"></i>'+'<i class="demo-icon icon-circle-empty"></i><i class="demo-icon icon-circle-empty"></i><i class="demo-icon icon-circle-empty"></i>';break;
+
+    //GIT    
+    case 8:kola=kola+'<i class="demo-icon icon-circle"></i>'+'<i class="demo-icon icon-circle-empty"></i><i class="demo-icon icon-circle-empty"></i><i class="demo-icon icon-circle-empty"></i><i class="demo-icon icon-circle-empty"></i>';break;  
     
-    
+            //HTML    
+    case 9:kola=kola+'<i class="demo-icon icon-circle"></i><i class="demo-icon icon-circle"></i><i class="demo-icon icon-circle"></i>'+'<i class="demo-icon icon-circle-empty"></i>';break;     
+
+    //CSS    
+    case 10:kola=kola+'<i class="demo-icon icon-circle"></i><i class="demo-icon icon-circle"></i><i class="demo-icon icon-circle"></i>'+'<i class="demo-icon icon-circle-empty"></i><i class="demo-icon icon-circle-empty"></i>';break;     
+
+    //Raspberry Pi
+    case 11:kola=kola+'<i class="demo-icon icon-circle"></i><i class="demo-icon icon-circle"></i><i class="demo-icon icon-circle"></i>'+'<i class="demo-icon icon-circle-empty"></i><i class="demo-icon icon-circle-empty"></i>';break;                                    
+
+    //Linux    
+    case 12:kola=kola+'<i class="demo-icon icon-circle"></i><i class="demo-icon icon-circle"></i>'+'<i class="demo-icon icon-circle-empty"></i><i class="demo-icon icon-circle-empty"></i><i class="demo-icon icon-circle-empty"></i>';break;                                
+
+    //JQuery    
+    case 13:kola=kola+'<i class="demo-icon icon-circle"></i>'+'<i class="demo-icon icon-circle-empty"></i><i class="demo-icon icon-circle-empty"></i><i class="demo-icon icon-circle-empty"></i><i class="demo-icon icon-circle-empty"></i>';break;    
+
+    //ee08    
+        case 14:kola=kola+'Praktyka: 100% <br> Teoria: 75%';break;   
+
+    //ee09     
+    case 15:kola=kola+'Praktyka: 100% <br> Teoria: 93%';break;
+
+    //PHP      
+    case 16:kola=kola+'<i class="demo-icon icon-circle"></i>'+'<i class="demo-icon icon-circle-empty"></i><i class="demo-icon icon-circle-empty"></i><i class="demo-icon icon-circle-empty"></i><i class="demo-icon icon-circle-empty"></i>';break;
+
+    //Światłowody    
+    case 17:kola=kola+'<i class="demo-icon icon-circle"></i><i class="demo-icon icon-circle"></i><i class="demo-icon icon-circle"></i>'+'<i class="demo-icon icon-circle-empty"></i><i class="demo-icon icon-circle-empty"></i>';break; 
+
+    //Angielski  
+    case 18:kola=kola+'<i class="demo-icon icon-circle"></i><i class="demo-icon icon-circle"></i><i class="demo-icon icon-circle"></i>'+'<i class="demo-icon icon-circle-empty"></i><i class="demo-icon icon-circle-empty"></i>';break;
+
+    //Sieci 
+    case 19:kola=kola+'<i class="demo-icon icon-circle"></i><i class="demo-icon icon-circle"></i><i class="demo-icon icon-circle"></i>'+'<i class="demo-icon icon-circle-empty"></i><i class="demo-icon icon-circle-empty"></i>';break;
+
+    //MySQL    
+    case 20:kola=kola+'<i class="demo-icon icon-circle"></i><i class="demo-icon icon-circle"></i>'+'<i class="demo-icon icon-circle-empty"></i><i class="demo-icon icon-circle-empty"></i><i class="demo-icon icon-circle-empty"></i>';break;    
+
+    default:kola='';                                
+    }    
+                                                                                        
+document.getElementById("kola"+n).innerHTML=kola;   
 }
 
-
-function Odwroc(n)
-{
- console.log("eee:"+n);     
-    
-    
-    
-}
