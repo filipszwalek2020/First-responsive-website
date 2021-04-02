99a0ebadd8fcf51323dd09f7b8008fd8a20c5f0b
Maciej Szwałek <maciejfilip.szwalek@gmail.com>
2021-04-02 13:22:09 +0200

    Poprawka animacji w JQuery

--- JS/default.js
+++ JS/default.js
@@ -1,72 +1,17 @@
-function Circles(n)
+function Circles(n,x)
 {
-var kola='';
-                                                         
+var kola='';                                                       
 switch(n)
-    {
-    //C++    
+{ 
     case 1:kola=kola+'<i class="demo-icon icon-circle"></i>'+'<i class="demo-icon icon-circle-empty"></i><i class="demo-icon icon-circle-empty"></i><i class="demo-icon icon-circle-empty"></i><i class="demo-icon icon-circle-empty"></i>';break;       
-
-    //JavaScript
-    case 2:kola=kola+'<i class="demo-icon icon-circle"></i>'+'<i class="demo-icon icon-circle-empty"></i><i class="demo-icon icon-circle-empty"></i><i class="demo-icon icon-circle-empty"></i><i class="demo-icon icon-circle-empty"></i>';break;
-
-    //Bootstrap    
-    case 3:kola=kola+'<i class="demo-icon icon-circle"></i>'+'<i class="demo-icon icon-circle-empty"></i><i class="demo-icon icon-circle-empty"></i><i class="demo-icon icon-circle-empty"></i><i class="demo-icon icon-circle-empty"></i>';break;  
-
-    //Python 
-    case 4:kola=kola+'<i class="demo-icon icon-circle"></i>'+'<i class="demo-icon icon-circle-empty"></i><i class="demo-icon icon-circle-empty"></i><i class="demo-icon icon-circle-empty"></i><i class="demo-icon icon-circle-empty"></i>';break;  
-
-    //Adobe Illustrator    
-    case 5:kola=kola+'<i class="demo-icon icon-circle"></i><i class="demo-icon icon-circle"></i>'+'<i class="demo-icon icon-circle-empty"></i><i class="demo-icon icon-circle-empty"></i><i class="demo-icon icon-circle-empty"></i>';break;  
-
-    //Adobe Photoshop     
-    case 6:kola=kola+'<i class="demo-icon icon-circle"></i><i class="demo-icon icon-circle"></i><i class="demo-icon icon-circle"></i>'+'<i class="demo-icon icon-circle-empty"></i><i class="demo-icon icon-circle-empty"></i>';break; 
-
-    //Adobe Premiere Pro
-    case 7:kola=kola+'<i class="demo-icon icon-circle"></i><i class="demo-icon icon-circle"></i>'+'<i class="demo-icon icon-circle-empty"></i><i class="demo-icon icon-circle-empty"></i><i class="demo-icon icon-circle-empty"></i>';break;
-
-    //GIT    
-    case 8:kola=kola+'<i class="demo-icon icon-circle"></i>'+'<i class="demo-icon icon-circle-empty"></i><i class="demo-icon icon-circle-empty"></i><i class="demo-icon icon-circle-empty"></i><i class="demo-icon icon-circle-empty"></i>';break;  
-    
-            //HTML    
-    case 9:kola=kola+'<i class="demo-icon icon-circle"></i><i class="demo-icon icon-circle"></i><i class="demo-icon icon-circle"></i>'+'<i class="demo-icon icon-circle-empty"></i>';break;     
-
-    //CSS    
-    case 10:kola=kola+'<i class="demo-icon icon-circle"></i><i class="demo-icon icon-circle"></i><i class="demo-icon icon-circle"></i>'+'<i class="demo-icon icon-circle-empty"></i><i class="demo-icon icon-circle-empty"></i>';break;     
-
-    //Raspberry Pi
-    case 11:kola=kola+'<i class="demo-icon icon-circle"></i><i class="demo-icon icon-circle"></i><i class="demo-icon icon-circle"></i>'+'<i class="demo-icon icon-circle-empty"></i><i class="demo-icon icon-circle-empty"></i>';break;                                    
-
-    //Linux    
-    case 12:kola=kola+'<i class="demo-icon icon-circle"></i><i class="demo-icon icon-circle"></i>'+'<i class="demo-icon icon-circle-empty"></i><i class="demo-icon icon-circle-empty"></i><i class="demo-icon icon-circle-empty"></i>';break;                                
-
-    //JQuery    
-    case 13:kola=kola+'<i class="demo-icon icon-circle"></i>'+'<i class="demo-icon icon-circle-empty"></i><i class="demo-icon icon-circle-empty"></i><i class="demo-icon icon-circle-empty"></i><i class="demo-icon icon-circle-empty"></i>';break;    
-
-    //ee08    
-        case 14:kola=kola+'Praktyka: 100% <br> Teoria: 75%';break;   
-
-    //ee09     
-    case 15:kola=kola+'Praktyka: 100% <br> Teoria: 93%';break;
-
-    //PHP      
-    case 16:kola=kola+'<i class="demo-icon icon-circle"></i>'+'<i class="demo-icon icon-circle-empty"></i><i class="demo-icon icon-circle-empty"></i><i class="demo-icon icon-circle-empty"></i><i class="demo-icon icon-circle-empty"></i>';break;
-
-    //Światłowody    
-    case 17:kola=kola+'<i class="demo-icon icon-circle"></i><i class="demo-icon icon-circle"></i><i class="demo-icon icon-circle"></i>'+'<i class="demo-icon icon-circle-empty"></i><i class="demo-icon icon-circle-empty"></i>';break; 
-
-    //Angielski  
-    case 18:kola=kola+'<i class="demo-icon icon-circle"></i><i class="demo-icon icon-circle"></i><i class="demo-icon icon-circle"></i>'+'<i class="demo-icon icon-circle-empty"></i><i class="demo-icon icon-circle-empty"></i>';break;
-
-    //Sieci 
-    case 19:kola=kola+'<i class="demo-icon icon-circle"></i><i class="demo-icon icon-circle"></i><i class="demo-icon icon-circle"></i>'+'<i class="demo-icon icon-circle-empty"></i><i class="demo-icon icon-circle-empty"></i>';break;
-
-    //MySQL    
-    case 20:kola=kola+'<i class="demo-icon icon-circle"></i><i class="demo-icon icon-circle"></i>'+'<i class="demo-icon icon-circle-empty"></i><i class="demo-icon icon-circle-empty"></i><i class="demo-icon icon-circle-empty"></i>';break;    
-
+    case 2:kola=kola+'<i class="demo-icon icon-circle"></i><i class="demo-icon icon-circle"></i>'+'<i class="demo-icon icon-circle-empty"></i><i class="demo-icon icon-circle-empty"></i><i class="demo-icon icon-circle-empty"></i>';break;
+    case 3:kola=kola+'<i class="demo-icon icon-circle"></i><i class="demo-icon icon-circle"></i><i class="demo-icon icon-circle"></i>'+'<i class="demo-icon icon-circle-empty"></i><i class="demo-icon icon-circle-empty"></i>';break;  
+    case 4:kola=kola+'<i class="demo-icon icon-circle"></i><i class="demo-icon icon-circle"></i><i class="demo-icon icon-circle"></i><i class="demo-icon icon-circle"></i>'+'<i class="demo-icon icon-circle-empty"></i>';break;  
+    case 5:kola=kola+'<i class="demo-icon icon-circle"></i><i class="demo-icon icon-circle"></i><i class="demo-icon icon-circle"></i><i class="demo-icon icon-circle"></i><i class="demo-icon icon-circle"></i><i class="demo-icon icon-circle"></i>';break;           
+    case 6:kola=kola+'Praktyka: 100% <br> Teoria: 75%';break;  
+    case 7:kola=kola+'Praktyka: 100% <br> Teoria: 93%';break;          
     default:kola='';                                
-    }    
-                                                                                        
-document.getElementById("kola"+n).innerHTML=kola;   
+}                                                                                           
+document.getElementById("kola"+x).innerHTML=kola;   
 }
 
